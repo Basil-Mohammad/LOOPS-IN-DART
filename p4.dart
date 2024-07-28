ece3a330ef96082be3c67c5ec3a9b17a4d71e193
Basil-Mohammad <134337008+Basil-Mohammad@users.noreply.github.com>
2024-07-28 19:48:34 +0300

    Add files via upload

---
+++ p4.dart
@@ -0,0 +1,78 @@
+
+// For Loop
+void main(){
+
+/*
+  for(int i =1; i<=100; i++){
+    
+
+    if(i % 2 != 0){
+
+      print("The number is : $i");
+
+    }
+    
+    
+
+
+  }
+  */
+
+  /*
+
+  for(int i =2; i<=100; i+=2){
+    
+
+      print("The number is : $i");
+
+
+  }
+  */
+  /*
+  for(int i =100; i>=1; i--){
+    
+
+      print("The number is : $i");
+
+
+  }
+*/
+// for each example
+/*
+List<String> name = ['ahmad','sara','mona','samer'];
+
+// for(int i = 0; i<= name.length-1; i++){
+//   print (name[i]);
+// }
+
+name.forEach((item)=>print(item));
+*/
+/*
+// for in loop
+
+List<String> name = ['ahmad','sara','mona','samer'];
+
+for (String element in name) {
+
+  print (element);
+  
+}
+*/
+
+// asMap()
+/*
+List<String> name = ['ahmad','sara','mona','samer'];
+
+name.asMap().forEach((index, value)=> print("$value index is $index"));
+*/
+
+// while loop
+/*
+int i =100;
+  while(i>=1){
+    print ("The number is : $i");
+    i--;
+  }
+  */
+  
+}
